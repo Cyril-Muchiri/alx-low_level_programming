4ef92f7831622bd221d333332393b5f1fcc2b74c
Cyril Muchiri <104130179+Cyril-Muchiri@users.noreply.github.com>
2023-06-15 14:01:21 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
-* Description: main - Prints out the string to stdout.
+/**
+* main - Prints out the string to stdout.
 * Return: 0 if success
 */
 int main(void)
-	/* Description: this is the main function*/
+	/* Description: this is the main function */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
