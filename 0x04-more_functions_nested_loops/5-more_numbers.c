38585f80c32a6bb1218688d06315e4cf50021dac
Cyril Muchiri <104130179+Cyril-Muchiri@users.noreply.github.com>
2023-06-23 22:42:48 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void);
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0;i < 10;i++)
 	{
 		int j;
 
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
