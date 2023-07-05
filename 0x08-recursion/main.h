49d6174ff63766bfe8989785631b73aff68f3fa2
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-07-05 23:33:20 +0300

    mandatory tasks complete

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description : This main.h file contains prototypes of the functions
+ *
+ */
+
+
+
+#include <string.h>
+#include <stdio.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int _putchar(char c);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
