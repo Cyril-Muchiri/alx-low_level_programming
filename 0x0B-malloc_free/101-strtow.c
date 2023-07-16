adfceda24552c4495746b0f19b25069084e65e50
Cyril Muchiri <104130179+Cyril-Muchiri@users.noreply.github.com>
2023-07-16 12:38:09 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,3 +7,25 @@
   *
   * Return: number of words
   */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+char **strtow(char *str)
+{
