11ccd1754de3c7c270cece0bda6d2ebc5db6df44
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-07-16 12:32:33 +0300

    advanced tasks

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * count_word - helper function to count the number of words in a string
+  * @s: string to evaluate
+  *
+  * Return: number of words
+  */
